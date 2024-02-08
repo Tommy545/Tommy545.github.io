59e0d63742be965265285ceeefd017b4568c0fa4
tommykinfu@gmail.com <tommykinfu@gmail.com>
2024-02-08 19:02:25 +0100

    finished home page

---
+++ Portfolio Project/index.js
@@ -0,0 +1,10 @@
+function transition() {
+    document.body.style.overflow = 'hidden';
+
+    document.getElementById("next").scrollIntoView({ behavior: "smooth" }); 
+};
+function btransition() {
+    document.body.style.overflow = 'hidden';
+
+    document.getElementById("back").scrollIntoView({ behavior: "smooth" }); 
+};
