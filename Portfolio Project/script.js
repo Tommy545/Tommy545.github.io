c1f0ceb28a896fc2814371ac7f5ddfd8899d1561
Tommy <tommykinfu@gmail.com>
2025-04-10 14:34:17 +0200

    SMTP

--- Portfolio Project/script.js
+++ Portfolio Project/script.js
@@ -1,7 +1,31 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: 'hostingsquire@gmail.com',
+        pass: 'secret!'
+    }
+});
+// https://stackoverflow.com/questions/271171/sending-emails-with-javascript
+// https://sendlayer.com/blog/how-to-send-email-with-javascript/
+// https://kinsta.com/blog/gmail-smtp-server/#what-is-smtp
+// https://mailtrap.io/blog/gmail-smtp/#How-to-configure-Gmail-SMTP-server-settings
+const mailOptions = {
+    from: 'hostingsquire@gmail.com',
+    to: 'hostingsquire@gmail.com',
+    subject: 'test',
+    text: 'test'
+};
+
 function transition() {
     document.body.style.overflow = 'hidden';
 
     document.getElementById("next").scrollIntoView({ behavior: "smooth" }); 
+
+    console.log("test")
 };
 function btransition() {
     document.body.style.overflow = 'hidden';
